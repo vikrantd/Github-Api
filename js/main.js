1bd190e494c89e435edfdbadddcd81555445c28e
Vikrant Dubey <vikrantdubey94@gmail.com>
2015-09-11 18:55:26 +0530

    Final

--- js/main.js
+++ js/main.js
@@ -8,22 +8,31 @@ app.controller('AppCtrl', function(  $scope,  $http ) {
 
 
 // declaring all variables to 0
-$scope.till_7_days = 0;
-$scope.till_24_hours = 0;
-$scope.pulls = 0;
+$scope.data =   { 
+                till_7_days : 0,
+                till_24_hours : 0,
+                pulls : 0
+                }
+
 var url = "https://api.github.com/repos/";
 
-var issue_data = [];
+$scope.issue_data = [];
 var opened_issues = [];
+
+
+
+
+
+
 // main function which retrieves the data from the api
+
 $scope.retrievedata = function(repo){
     
-    var issue_data = [];
+    $scope.issue_data = [];
     var opened_issues = [];
+    
     $scope.loading = "processing"; //lodaing variable that defines the current state 
-    $scope.till_7_days = 0;
-    $scope.till_24_hours = 0;
-    $scope.pulls = 0;
+   
     
     var i= 1;
     var j = 1;
@@ -38,6 +47,9 @@ $scope.retrievedata = function(repo){
 
 
 
+
+
+
 // get_issues function to get the issues of last 7 days
 
 var get_issues = function(iteration, repo){
@@ -46,10 +58,7 @@ var get_issues = function(iteration, repo){
     before7days.setDate(before7days.getDate()-7);
     before7days = before7days.toISOString();  //converting the date to iso string to pass it to the api 
 
-    
-    
-    
-    
+
     
 
     //http get request to get the issue data in JSON format
@@ -60,17 +69,21 @@ var get_issues = function(iteration, repo){
         iteration += 1;
         results.data.forEach(function(entry)
         {
-            issue_data.push(entry);
+            $scope.issue_data.push(entry);
         }); // pussing the objetcs in a temp var
-        console.log(issue_data);
+        
+        console.log($scope.issue_data);
+        
         if(results.data.length==100)
         {
            get_issues(iteration , repo);
         }
 
-        else{
+        else
+        {
             $scope.loading = "done";
-            issue_data.forEach(function(entry) {
+
+            $scope.issue_data.forEach(function(entry) {
                 
                 var created_time = Date.parse(entry.created_at);
                 var _before7days = Date.parse(before7days);
@@ -79,7 +92,7 @@ var get_issues = function(iteration, repo){
                 if( created_time > _before7days && !entry.pull_request)
                 
                 {
-                    $scope.till_7_days ++ ;
+                    $scope.data.till_7_days ++ ;
 
                     var before1day = new Date();
                     before1day.setDate(before1day.getDate()-1);
@@ -89,8 +102,8 @@ var get_issues = function(iteration, repo){
 
                     if( created_time > _before1day)
                     {
-                        $scope.till_24_hours ++ ;
-                        //console.log($scope.till_24_hours);
+                        $scope.data.till_24_hours ++ ;
+                        console.log($scope.data.till_24_hours);
 
                     }
 
@@ -109,16 +122,22 @@ var get_issues = function(iteration, repo){
 
 }
 
+
+
+
+
+
+
+
+
 //function to gt the number of pulls so that we can minus it from the pulls  to get the exact number.
 var getpulls = function(iteration, repo){
 
 
-    $http.get(url + repo + '/pulls?page=' + iteration + '&per_page=100').then(function(results){
-
-        
+        $http.get(url + repo + '/pulls?page=' + iteration + '&per_page=100').then(function(results){      
         iteration += 1;
-        $scope.pulls += results.data.length;
-        console.log($scope.pulls);
+        $scope.data.pulls += results.data.length;
+        console.log($scope.data.pulls);
         if(results.data.length==100)
         {
            get_issues(iteration , repo);
@@ -129,6 +148,15 @@ var getpulls = function(iteration, repo){
     
 
 }
+
+
+
+
+
+
+
+
+
 //function to get basic data about repo and total number of issues
 var gettotal = function(repo){
 
@@ -140,6 +168,7 @@ var gettotal = function(repo){
       $scope.loading = "error";
     })
 }
+
 
 
 
